18caa484b84b4ff4d63c4c9581bbf40ae40d5a56
DESKTOP-A5VNKE5\jackb <jack.burt1@hotmail.co.uk>
2018-12-06 14:06:22 +0000

    Commented out postmapping that caused error

--- src/main/java/com3014/league/controller/leagueController.java
+++ src/main/java/com3014/league/controller/leagueController.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
